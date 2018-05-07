4a47d86ad67405c7eae53c81f0e92f08fe7ecc95
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-07 19:44:34 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -2,7 +2,7 @@
   def game_hash
    game_hash = {
     :home =>
-      {team_name: "Brooklyn",
+      {team_name: "Brooklyn Nets",
        colors:["Black", "White"],
        :players => {
          "Alan Anderson" => {
