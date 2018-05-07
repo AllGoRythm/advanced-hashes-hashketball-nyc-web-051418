8b2263153ff15b10ecdea21a4c46a60e607905b6
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-07 18:39:26 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -124,6 +124,7 @@ def num_points_scored(player_name)
         data.each do |name, info|
           if name == player_name
             :points
+            binding.pry
           end
         end
       end
