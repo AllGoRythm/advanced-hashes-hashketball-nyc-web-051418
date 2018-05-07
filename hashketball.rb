c433e1338227a8494eb0276e3118d1efcad43edf
Kurt Bauer <kurtbauer55@protonmail.com>
2018-05-07 20:26:55 +0000

    automatically backed up by learn

--- hashketball.rb
+++ hashketball.rb
@@ -154,7 +154,7 @@ def player_numbers(team)
   collect = []
   game_hash.select do |key, value|
     if game_hash[key][:team_name].include?(team)
-      collect << collect += game_hash[key][:players][:number]
+      collect += game_hash[key][:players][:number]
     end
   end
   collect
